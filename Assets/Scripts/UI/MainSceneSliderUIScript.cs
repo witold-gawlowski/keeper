67f004a4e11ab3cb26fe26ccda5e75c414ed4c81
Witold Gawłowski <witek.gawlowski@gmail.com>
2021-12-16 15:15:39 +0100

    .

--- Assets/Scripts/UI/MainSceneSliderUIScript.cs
+++ Assets/Scripts/UI/MainSceneSliderUIScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MainSceneSliderScript : MonoBehaviour
+public class MainSceneSliderUIScript : MonoBehaviour
 {
     [SerializeField] Image marker;
     [SerializeField] RectTransform parent;

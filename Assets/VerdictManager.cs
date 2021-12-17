e0e5df522f57d4e32b1f15c0c3d249e26198bc8d
Witold Gawłowski <witek.gawlowski@gmail.com>
2021-12-17 13:05:50 +0100

    .

--- Assets/VerdictManager.cs
+++ Assets/VerdictManager.cs
@@ -6,22 +6,54 @@ public class VerdictManager : Singleton<VerdictManager>
 {
     public System.Action<bool> verdictEvent;
 
+    [SerializeField] private float numberOfVerdictPointPerUnit = 5.0f;
+    [SerializeField] private float dispersionRadius = 0.4f;
+
+    private ObjectPool hitPool;
     private PolygonCollider2D levelCollider;
+    int pathCount;
+    private void Awake()
+    {
+        hitPool = GetComponentInChildren<ObjectPool>();
+    }
     private void Start()
     {
-        GameObject levelObject = MainSceneManager.Instance.LevelObject;
-        levelCollider = levelObject.GetComponent<PolygonCollider2D>();
-        foreach(var p in levelCollider.points)
-        {
-            Debug.Log(p);
-        }
+        InitLevelCollider();
     }
     private void OnEnable()
     {
         MainSceneUIManager.Instance.verdictPressedEvent += StartVerdict;
     }
+    private void OnDisable()
+    {
+        if (MainMenuUIManager.Instance)
+        {
+            MainSceneUIManager.Instance.verdictPressedEvent -= StartVerdict;
+        }
+    }
+    void InitLevelCollider()
+    {
+        GameObject levelObject = MainSceneManager.Instance.LevelObject;
+        levelCollider = levelObject.GetComponent<PolygonCollider2D>();
+        pathCount = levelCollider.pathCount;
+    }
     void StartVerdict()
     {
+        Debug.Log("Start Verdict");
+        for(int i=0; i<pathCount; i++)
+        {
+            var path = levelCollider.GetPath(i);
+            Debug.Log("pathcount " + path.Length);
+            for (int j=0; j<path.Length; j++)
+            {
+                var pointA = path[j];
+                var pointB = path[(j + 1) % path.Length];
+                var length = (pointB - pointA).magnitude;
+                var numberOfVerdictPoints = length * numberOfVerdictPointPerUnit;
+                var unitVector = (pointB - pointA).normalized;
+                hitPool.Spawn(path[j]);
 
+            }
+        }
     }
 }

30ed96fedc94d51eecfe7be995606f5c8cdde5f1
Witold Gawłowski <witek.gawlowski@gmail.com>
2022-01-20 11:07:31 +0100

    new MapSO previewing

--- Assets/Editor/MapSOEditor.cs
+++ Assets/Editor/MapSOEditor.cs
@@ -17,8 +17,29 @@ public class MapSOEditor : Editor
         var rHeight = (int)rect.height;
         var pixData = sprite.texture.GetPixels(x, y, rWidth, rHeight);
         var newTex = new Texture2D((int)rect.width, (int)rect.height);
+        var scaleFactor = Mathf.Min(width / rWidth, height / rHeight);
         newTex.SetPixels(pixData);
+        newTex.filterMode = FilterMode.Point;
         newTex.Apply();
+        newTex = ScaleTexture(newTex, rWidth * scaleFactor, rHeight* scaleFactor);
         return newTex;
+    }
+    private Texture2D ScaleTexture(Texture2D source, int targetWidth, int targetHeight)
+    {
+        Texture2D result = new Texture2D(targetWidth, targetHeight, source.format, false);
+        float incX = (1.0f / (float)targetWidth);
+        float incY = (1.0f / (float)targetHeight);
+        for (int i = 0; i < result.height; ++i)
+        {
+            for (int j = 0; j < result.width; ++j)
+            {
+                int x = (int)((float)j / (float)result.width * source.width);
+                int y = (int)((float)i / (float)result.height * source.height);
+                Color newColor = source.GetPixel(x, y);
+                result.SetPixel(j, i, newColor);
+            }
+        }
+        result.Apply();
+        return result;
     }
 }
